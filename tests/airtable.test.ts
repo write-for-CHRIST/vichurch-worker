8c48ddbb701f29d5d6cb5b7928546362bab277ee
nampdn <nampdn@protonmail.com>
2019-02-19 05:59:51 +0700

    test(airtable): not display content

--- tests/airtable.test.ts
+++ tests/airtable.test.ts
@@ -2,10 +2,7 @@ import {base, getAllProfiles} from '../src/airtable';
 
 describe('airtable', () => {
   it('fetch all profiles from airtable', async done => {
-    try {
-      const profiles = await getAllProfiles();
-      console.log(profiles);
-      done();
-    } catch (err) {}
+    const profiles = await getAllProfiles();
+    done();
   });
 });
